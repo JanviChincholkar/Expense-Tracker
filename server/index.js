95984bbc23ae2a4d7f72e9d0190b24b138f7609d
Janvi Chincholkar <janvichincholkar14@gmail.com>
2024-07-28 16:48:33 +0530

    transactioncard

--- server/index.js
+++ server/index.js
@@ -3,22 +3,18 @@ import mongoose from "mongoose";
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
+import { PostSignup,PostLogin } from './controllers/user.js';
+import { PostTransaction,getTransactions ,deleteTransaction} from './controllers/transaction.js';
 
-import user from './models/User.js'
-import Transaction from './models/Transaction.js';
 const app = express ();
 app.use(express.json());
 app.use(cors());
 
-import { PostSignup, PostLogin } from './controllers/user.js';
-import { PostTransaction,  getTransactions } from './controllers/transaction.js';
-
-
 //connect mongodb
-const connectDB =async ()=>{
+ const connectDB =async ()=>{
     const conn = await mongoose.connect(process.env.MONGO_URL)
     if(conn){
-        console.log(`mongodb connected sucessfully.....`);
+        console.log(`mongodb is connected sucessfully`);
     }
  }
  connectDB();
@@ -28,16 +24,15 @@ const connectDB =async ()=>{
      message:`welcome to expense tracker API`
     })
  })
-
  app.post("/signup", PostSignup )
- app.post("/login",PostLogin)
- app.post("/transaction", PostTransaction)
- app.get("/transactions",getTransactions )
+app.post("/login",PostLogin)
+app.post("/transaction",PostTransaction)
+app.get("/transactions",getTransactions )
+app.delete("/transaction/:id",deleteTransaction)
 
-   
 
 
 const PORT = process.env.PORT || 5000;
-  app.listen (PORT,()=>{
+  app.listen (PORT,(req,res)=>{
     console.log(`server is running on port ${PORT}`);
   })
