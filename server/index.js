1f359eae8e7f21b41a9309d6500197251f4ee4ae
Janvi Chincholkar <janvichincholkar14@gmail.com>
2024-07-29 20:17:42 +0530

    fiile change

--- server/index.js
+++ server/index.js
@@ -1,38 +1,32 @@
-import express  from 'express';
-import mongoose from "mongoose";
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import { PostSignup,PostLogin } from './controllers/user.js';
-import { PostTransaction,getTransactions ,deleteTransaction} from './controllers/transaction.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express ();
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-//connect mongodb
- const connectDB =async ()=>{
-    const conn = await mongoose.connect(process.env.MONGO_URL)
-    if(conn){
-        console.log(`mongodb is connected sucessfully`);
-    }
- }
- connectDB();
+const connectDB = async () =>{
+    const conn = await mongoose.connect(process.env.MONGODB_URL)
 
- app.get('/',(req,res)=>{
-    res.json({
-     message:`welcome to expense tracker API`
-    })
- })
- app.post("/signup", PostSignup )
-app.post("/login",PostLogin)
-app.post("/transaction",PostTransaction)
-app.get("/transactions",getTransactions )
-app.delete("/transaction/:id",deleteTransaction)
+    if (conn) {
+        console.log(`MongoDB connected successfully ✅`);
+      }
+    };
+    connectDB();
 
+    app.get('/', (req, res) => {
+        res.json({
+          message: `Welcome to Expense Tracker API`
+        })
+      })
+      
 
 
 const PORT = process.env.PORT || 5000;
-  app.listen (PORT,(req,res)=>{
-    console.log(`server is running on port ${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   })
